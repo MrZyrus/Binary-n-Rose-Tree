14f1ccb0e4e306e6d327427985133e851614ec8c
MrZyrus <kaiser705@gmail.com>
2016-06-30 14:48:02 -0400

    Updated trees.rb, commenting.

--- trees.rb
+++ trees.rb
@@ -2,7 +2,7 @@ require_relative 'nodos.rb'
 require_relative 'mod_bfs.rb'
 require_relative 'mod_fold.rb'
 
-class Tree
+class Tree	#Super class, since all trees have common stuff, notice these aren't actually trees, they're more like nodes.
 	include BFS
 	include DFS
 
@@ -16,36 +16,36 @@ class Tree
 		@value = x
 	end
 
-	def mutar(x)
-		self.setter(self.getter.mutar(x))
+	def mutar(x)	#Now here, we can mutate trees, when you mutate trees, we actually change the values.
+		self.setter(self.valor.mutar(x))	#So we use the setter.
 	end
 end
 
 class ArbolBinario < Tree
-	@left
+	@left	#Binary trees only have two childs.
 	@right
 
-	def initialize(x, l = nil, r = nil)
+	def initialize(x, l = nil, r = nil)	#nil childs if no childs.
 		@value = x
 		@left = l
 		@right = r
 	end
 
-	def each
+	def each	#Just return both childs in order, left then right, only if they exist.
 		yield @left if @left
 		yield @right if @right
 	end
 end
 
 class ArbolRosa < Tree
-	@childs
+	@childs	#Rose trees have an array of childs, any number of them.
 
-	def initialize(x, *c)
+	def initialize(x, *c)	#No childs means empty array.
 		@value = x
 		@childs = c
 	end
 
-	def each
+	def each	#This one is simpler, just return the elements of the array.
 		@childs.each {|i| yield i} if @childs
 	end
 end
