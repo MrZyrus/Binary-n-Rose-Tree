0382a2e1cb8b9afabfa3f7bfe640197126d87e26
MrZyrus <kaiser705@gmail.com>
2016-06-30 14:37:09 -0400

    Updated mod_bfs.rb, commenting

--- mod_bfs.rb
+++ mod_bfs.rb
@@ -1,17 +1,17 @@
 module BFS
-	def bfs
-		q = Queue.new
-		q.push(self)
+	def bfs	#bfs method, traverses the tree using Breadth First Search
+		q = Queue.new	#Easy implementation using a queue
+		q.push(self)	#Start with the root node
 		while not q.empty? do
 			r = q.pop
-			r.each {|i| q.push(i)}
+			r.each {|i| q.push(i)}	
 			yield r
 		end
 	end
 
-	def recoger(block)
+	def recoger(block)	#Traverses the tree using bfs then returns an array, notice block is explicit
 		a = []
-		self.bfs {|i| a.push(i) if block.call(i)}
+		self.bfs {|i| a.push(i) if block.call(i)}	#The array returned is just the nodes which obey the block sent
 		return a
 	end
 end
